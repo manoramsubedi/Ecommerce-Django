8b254ecedaf9f8bc230c193cdbe54d27cf7cef07
Manoram <manoramsubedi7@gmail.com>
2024-08-30 23:23:45 +0545

    some url fixed

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from category.models import Category
 
+from django.urls import reverse
+
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=100, unique=True)
@@ -12,9 +14,14 @@ class Product(models.Model):
     is_availiable = models.BooleanField(default=True)
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    
+
     created_date = models.DateTimeField(auto_now_add=True, null=True)
     modified_date = models.DateTimeField(auto_now_add=True, null=True)
 
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
     def __str__(self):
         return self.product_name
+
+
--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 
+
 # Create your views here.
 def store(request, slug=None):
     categories = None
