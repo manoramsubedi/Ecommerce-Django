61d449a6f0a70e55b085b09a7078a3e712192d48
Manoram <manoramsubedi7@gmail.com>
2024-08-30 23:02:29 +0545

    single view product

--- store/views.py
+++ store/views.py
@@ -17,6 +17,15 @@ def store(request, slug=None):
         products = Product.objects.all().filter(is_availiable=True)
         product_count = products.count()
 
-
     context = {"products":products, "product_count":product_count}
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {'single_product':single_product}
+    return render(request, 'store/product-detail.html', context)
+
