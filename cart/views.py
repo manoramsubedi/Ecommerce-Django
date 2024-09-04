ebcc6f967f9b1bf796d765233bbe549a273cf481
Manoram <manoramsubedi7@gmail.com>
2024-09-04 16:59:07 +0545

    cart add-remove

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from store.models import Product
 from .models import Cart, CartItems
 
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 
 def _cart_id(request):
@@ -64,3 +65,23 @@ def cart(request):
         'grand_total': grand_total,
     }
     return render(request, "store/cart.html", context)
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItems.objects.get(product=product, cart=cart)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItems.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
